bc93e070f2b9134b35e8b19225811a732b808211
Ayush Dhingra <ayush.dhingra@luminos.software>
2025-01-15 11:47:07 +0530

    final commit assignment done

--- src/show/screens/session/Homepage/style.js
+++ src/show/screens/session/Homepage/style.js
@@ -1,18 +1,9 @@
 import {StyleSheet} from 'react-native';
 import {sizes} from 'Theme';
 
-const STICKY_HEADER_HEIGHT = sizes.layout.headerHeight + sizes.padding * 4;
-
 export default StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1f2125',
-  },
-  searchContainer: {
-    position: 'absolute',
-    top: STICKY_HEADER_HEIGHT,
-    left: 0,
-    right: 0,
     backgroundColor: '#1f2125',
   },
   googleTitle: {
